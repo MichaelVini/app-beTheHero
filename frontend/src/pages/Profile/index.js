0638f0357e0485f6b6566ed4620d7d3b16a235b5
Michael <michaelvini62@gmail.com>
2020-07-14 21:02:35 -0300

    add logout function

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import logoImg from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import  { Link, useHistory }  from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import './style.css';
 import api from '../../services/api';
 
+
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
+    const history = useHistory();
 
     //buscar o nome da ong no storage do navegador.
     const ongName = localStorage.getItem('ongName');
@@ -43,13 +45,19 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="" />
                 <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="incidents/new">Cadastrar novo caso.</Link>
-                <button type="button"> <FiPower size={18} color="#E02041" /> </button>
+                <button onClick={handleLogout} type="button"> <FiPower size={18} color="#E02041" /> </button>
             </header>
 
             <h1>Casos cadastrados</h1>
