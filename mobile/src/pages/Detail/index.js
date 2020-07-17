6d9f1b707146ba2710b1c6ae299a6e74a0cd1b76
Michael <michaelvini62@gmail.com>
2020-07-17 20:22:40 -0300

    add loadIncidents

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -3,32 +3,37 @@ import { View, Image, TouchableOpacity, Text, Linking } from 'react-native';
 import styles from './styles';
 import logoImg from '../../assets/logo.png';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import * as MailComposer from 'expo-mail-composer';
 
 export default function Detail() {
     const navigation = useNavigation();
-    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de 150 reais';
-    
-    function navigateToIncidents(){
+    const route = useRoute();
+    const incident = route.params.incident;
+
+    const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}"
+     com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}.`;
+
+   
+    function navigateToIncidents() {
         navigation.navigate('Incidents');
     }
 
     function sendMail() {
         MailComposer.composeAsync({
             //Assunto do email
-            subject: 'Herói do caso: Cadelinha atropelada',
+            subject: `Herói do caso: ${incident.title}`,
             // Pra quem será enviado
-            recipients: ['michaelvini62@gmail.com'],
+            recipients: [incident.email],
             //Conteúdo da mensagem
             body: message,
         })
     }
 
-    function sendWhatsapp(){
+    function sendWhatsapp() {
         Linking.openURL(`whatsapp://send?phone=55012982317576&text=${message}`);
     }
-    
+
     return (
         <View style={styles.container}>
 
@@ -41,13 +46,15 @@ export default function Detail() {
 
             <View style={styles.incident}>
                 <Text style={styles.incidentProperty, { marginTop: 0 }}>ONG:</Text>
-                <Text style={styles.incidentValue}>APAD</Text>
+                <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
                 <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
 
                 <Text style={styles.incidentProperty}>VALOR:</Text>
-                <Text style={styles.incidentValue}>R$ 120,00 </Text>
+                <Text style={styles.incidentValue}>
+                    {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}
+                </Text>
             </View>
 
             <View style={styles.contactBox}>
@@ -66,7 +73,7 @@ export default function Detail() {
                     </TouchableOpacity>
                 </View>
             </View>
-    
+
 
         </ View>
     );
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -10,24 +10,40 @@ import api from '../../services/api'
 export default function Incidents() {
     const navigation = useNavigation();
     const [incidents, setIncidents] = useState([]);
-    const [total, setTotal] = useState(0); 
+    const [total, setTotal] = useState(0);
+    const [page, setPage]= useState(1);
+    const [loading, setLoading] = useState(false);
 
 
-    function navigateToDetail() {
-        navigation.navigate('Detail');
+    function navigateToDetail(incident) {
+        navigation.navigate('Detail', { incident });
     }
-    
+
     async function loadIncidents() {
-        const response = await api.get('incidents');
+        if(loading) {
+            return;
+        }
         
+        if(total>0 && incidents.length === total) {
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get('incidents', {
+            params: {page}
+        });
+
         //buscar dados do backend
-        setIncidents(response.data);
+        setIncidents([...incidents, ...response.data]);
         setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
     }
 
     useEffect(() => {
 
-       loadIncidents();
+        loadIncidents();
 
     }, []);
 
@@ -50,14 +66,16 @@ export default function Incidents() {
                 keyExtractor={incident => String(incident.id)}
                 //Tirar a barra de scrool
                 showsVerticalScrollIndicator={false}
+                onEndReached = {loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
 
                         <Text style={styles.incidentProperty}>ONG:</Text>
-                <Text style={styles.incidentValue}>{ incident.name }</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>{ incident.title }</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
                         <Text style={styles.incidentValue}>
@@ -66,7 +84,7 @@ export default function Incidents() {
 
                         <TouchableOpacity
                             style={styles.detailsButton}
-                            onPress={navigateToDetail}
+                            onPress={() => navigateToDetail(incident)}
                         >
                             <Text style={styles.detailsButtonText}> Ver mais detalhes</Text>
                             <Feather name="arrow-right" size={16} color="#E02041" />
